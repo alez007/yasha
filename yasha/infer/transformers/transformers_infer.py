b997c7e3db7e423175c6cafdc40f73f011c66e7c
Alex Margarit <alex@yasha.local>
2025-10-28 16:02:27 +0000

    started transformers tts

--- yasha/infer/transformers/transformers_infer.py
+++ yasha/infer/transformers/transformers_infer.py
@@ -1,6 +1,7 @@
+import logging
 from typing import Annotated
 import sys
-from transformers import pipeline, AutomaticSpeechRecognitionPipeline
+from transformers import pipeline, AutomaticSpeechRecognitionPipeline, TextToAudioPipeline, PretrainedConfig
 import torch
 
 from yasha.infer.infer_config import ModelUsecase, YashaModelConfig, SpeechRequest
@@ -8,43 +9,34 @@ from fastapi import FastAPI, Form, HTTPException, Request
 from http import HTTPStatus
 from vllm.entrypoints.openai.protocol import ChatCompletionRequest, EmbeddingRequest, TranscriptionRequest, TranscriptionResponse, TranslationRequest
 
+logger = logging.getLogger("ray")
 
 class TransformersInfer():
-    _transformers_usecases = []
+    _transformers_usecases = [ModelUsecase.tts]
 
     @staticmethod
     def check_transformers_support(model_config: YashaModelConfig) -> Exception|None:
         if model_config.use_vllm is False and model_config.usecase in TransformersInfer._transformers_usecases:
             raise Exception("transformers is only supported for (%s) models", ", ".join(TransformersInfer._transformers_usecases))
     
-    def create_pipeline(self) -> Exception|AutomaticSpeechRecognitionPipeline:
-        device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        model_usecase = self.model_config.usecase
-        model = self.model_config.model
-
-        match model_usecase:
-            case ModelUsecase.transcription:
-                return pipeline(
-                    task="automatic-speech-recognition",
-                    model=model,
-                    device=device,
-                    dtype=torch.float16
-                )
-            case _:
-                raise Exception("model usecase not supported with transformers")
-
     def __init__(self, model_config: YashaModelConfig):
         self.check_transformers_support(model_config)
 
         self.model_config = model_config
-
-        self.pipeline = self.create_pipeline()
     
     async def start(self):
-        await self.init_serving_chat()
+        self.serving_speech = await self.init_serving_speech()
     
-    async def init_serving_chat(self):
-        self.serving_chat = None
+    async def init_serving_speech(self) -> TextToAudioPipeline|None:
+        logger.info("init serving speech with model: %s", self.model_config.name)
+        return pipeline(
+            task="text-to-audio",
+            model=self.model_config.model,
+            config=PretrainedConfig(
+                device=self.device
+            ),
+            dtype=torch.float16
+        ) if self.model_config.usecase is ModelUsecase.tts else None
 
     async def create_chat_completion(self, request: ChatCompletionRequest, raw_request: Request):
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND.value,
