f1cf00de84a5a886a8cd8cd6bca0cca451641d9a
Alex Margarit <alex@yasha.local>
2025-11-07 15:08:26 +0000

    bark now working

--- yasha/plugins/tts/bark.py
+++ yasha/plugins/tts/bark.py
@@ -8,6 +8,8 @@ from vllm import SamplingParams
 import logging
 from collections.abc import AsyncGenerator
 import base64
+
+from vllm.entrypoints.openai.serving_models import create_error_response
 from yasha.infer.infer_config import SpeechResponse, SpeechRequest, RawSpeechResponse, YashaModelConfig
 from vllm.entrypoints.openai.protocol import ErrorInfo, ErrorResponse
 import wave
@@ -17,7 +19,7 @@ from yasha.plugins.base_plugin import BasePlugin
 from transformers import BarkModel, BarkProcessor
 from yasha.infer.infer_config import SpeechResponse, SpeechRequest, RawSpeechResponse
 from collections.abc import AsyncGenerator
-import struct
+from scipy.io.wavfile import write as write_wav
 
 logger = logging.getLogger("ray")
 
@@ -34,32 +36,17 @@ class ModelPlugin(BasePlugin):
         logger.info("started generation: %s with voice: %s to device %s", input, voice, self.device)
 
         inputs = self.processor(input, voice_preset=voice).to(device=self.device)
-        speech_output = self.model.generate(**inputs).cpu().numpy()
-        logger.info("speech_output 1: %s", speech_output)
         
-        buf = io.BytesIO()
-        with wave.open(buf, "wb") as wf:
-            wf.setnchannels(1)
-            wf.setsampwidth(2)
-            wf.setframerate(24000)
-            logger.info("got some audio bytes for wav: %s", speech_output)   # PCM16 bytes from SNAC
-            wf.writeframes(speech_output)
-                
+        sample_rate = self.model.generation_config.sample_rate
 
-        return RawSpeechResponse(audio=buf.getvalue(), media_type="audio/wav")
-        # if stream_format=="sse":
-        #     return self.generate_sse(input, voice, request_id)
-        # else:
-        #     buf = io.BytesIO()
-        #     with wave.open(buf, "wb") as wf:
-        #         wf.setnchannels(1)
-        #         wf.setsampwidth(2)
-        #         wf.setframerate(24000)
-        #         async for audio_bytes in self.generate_audio_bytes_async(input, voice, request_id):
-        #             logger.info("got some audio bytes for wav: %s", audio_bytes)   # PCM16 bytes from SNAC
-        #             wf.writeframes(audio_bytes)
-            
-        #     return RawSpeechResponse(audio=buf.getvalue(), media_type="audio/wav")
+        if stream_format=="sse":
+            return create_error_response("sse stream format not supported")
+        else:
+            buf = io.BytesIO()
+            speech_output = self.model.generate(**inputs).cpu().numpy().squeeze()
+            write_wav(buf, rate=sample_rate, data=speech_output)
+
+            return RawSpeechResponse(audio=buf.getvalue(), media_type="audio/wav")
 
         
     
