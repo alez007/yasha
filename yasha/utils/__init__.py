410396a0b1ad4c34a7dcc911dac3d6fb63026bdb
Alex Yasha <alex@yasha.local>
2025-12-14 09:51:16 +0000

    update

--- yasha/utils/__init__.py
+++ yasha/utils/__init__.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from fastapi import Request
 import uuid
+import os
+import requests
 
 def random_uuid() -> str:
     return str(uuid.uuid4().hex)
@@ -10,4 +12,12 @@ def base_request_id(raw_request: Optional[Request], default: Optional[str] = Non
     default = default or random_uuid()
     if raw_request is None:
         return default
+
+def download(url: str, file_path: str, overwrite: bool = False):
+    if os.path.isfile(file_path) is False:
+        get_response = requests.get(url, stream=True)
+        with open(file_path, 'wb') as f:
+            for chunk in get_response.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
 
